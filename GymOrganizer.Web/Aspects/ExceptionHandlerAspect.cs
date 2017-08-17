10ee1662bbaecb1d9e0a67b2dda5f7df72809d03
Alan Jagar <alan.jagar@hotmail.com>
2017-08-17 12:44:28 -0700

    Initial project setup (Web & Auth) Post sharp ExceptionHandler implemented

---
+++ GymOrganizer.Web/Aspects/ExceptionHandlerAspect.cs
@@ -0,0 +1,30 @@
+﻿using GymOrganizer.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GymOrganizer.Web.Aspects
+{
+    [PSerializable]
+    public class ExceptionHandlerAspect : OnExceptionAspect
+    {
+        
+        public override void OnException(MethodExecutionArgs args)
+        {
+            if(args.Instance != null)
+            {
+                var logger = (args.Instance as BaseController).GetLogger<ExceptionHandlerAspect>();
+                logger.LogInformation($"Alan - Exception handled {args.Exception.Message}");
+            }
+            args.FlowBehavior = FlowBehavior.Return;
+            args.ReturnValue = new BadRequestResult();
+        }
+    }
+}
---
+++ GymOrganizer.Web/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymOrganizer.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public BaseController(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public ILogger<T> GetLogger<T>()
+        {
+            return _loggerFactory.CreateLogger<T>();
+        }
+    }
+}
