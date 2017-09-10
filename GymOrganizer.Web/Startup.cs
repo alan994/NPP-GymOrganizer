2d020b14711b89a4057b6efd4b0cff2977c23be3
Alan Jagar <alan.jagar@hotmail.com>
2017-09-10 09:43:43 +0200

    Web api finished

--- GymOrganizer.Web/Startup.cs
+++ GymOrganizer.Web/Startup.cs
@@ -8,14 +8,32 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using GymOrganizer.Data.Db;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using GymOrganizer.Web.Utils;
+using GymOrganizer.Web.Services;
 
 namespace GymOrganizer.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
+            this.Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -23,20 +41,48 @@ namespace GymOrganizer.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            string connectionString = this.Configuration["Data:ConnectionString"];
+
+            services.AddDbContext<GymOrganizerContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.MigrationsAssembly("GymOrganizer.Data"));
+            });
+
+            services.AddTransient(typeof(UserService));
+            services.AddTransient(typeof(ScheduleService));
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                    new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling =
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, GymOrganizerContext db)
         {
             loggerFactory.AddConsole(LogLevel.Debug);
+
+            db.Database.Migrate();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            AutoMapperConfiguration.Configure();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
---
+++ GymOrganizer.Web/ViewModels/ScheduleVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymOrganizer.Web.ViewModels
+{
+    public class ScheduleVM
+    {
+        public Guid Id { get; set; }        
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public UserVM User { get; set; }
+    }
+}
